d42e07d568a956a1d412183c16025ef8bb0c8a05
Max <bloodmailvol1@gmail.com>
2026-05-25 18:11:04 +0300

    генерация html с помощью java script
    сначала подгружаются контейнеры под каждый тип, потом в каждый контейнер по типу подгружаются классы

---
+++ data/data.js
@@ -0,0 +1,152 @@
+const types = [{
+  nameRU: "Воины",
+  nameJS: "js-warriors-container"
+}, {
+  nameRU: "Маги",
+  nameJS: "js-mages-container"
+}, {
+  nameRU: "Монахи",
+  nameJS: "js-fighters-container"
+}, {
+  nameRU: "Стрелки",
+  nameJS: "js-gunners-container"
+}, {
+  nameRU: "Ассасины",
+  nameJS: "js-assassins-container"
+}, {
+  nameRU: "Творцы",
+  nameJS: "js-specialists-container"
+}, {
+  nameRU: "Рыцари-хранители",
+  nameJS: "js-guardianKnights-container"
+}];
+
+const warriors = [{
+  name: "Берсерк",
+  backgroundImg: "images/warriors/berserker/BG_Berserker.png",
+  img: "images/warriors/berserker/Warrior_Berserker.png"
+}, {
+  name: "Сокрушитель",
+  backgroundImg: "images/warriors/destroyer/BG_Destroyer.png",
+  img: "images/warriors/destroyer/Warrior_Destroyer.png"
+}, {
+  name: "Страж",
+  backgroundImg: "images/warriors/gunlancer/BG_Gunlancer.png",
+  img: "images/warriors/gunlancer/Warrior_Gunlancer.png"
+}, {
+  name: "Паладин",
+  backgroundImg: "images/warriors/paladin/BG_Paladin.png",
+  img: "images/warriors/paladin/Warrior_Paladin.png"
+}, {
+  name: "Валькирия",
+  backgroundImg: "images/warriors/slayer/BG_Slayer.png",
+  img: "images/warriors/slayer/Warrior_Slayer.png"
+}, {
+  name: "Храмовница",
+  backgroundImg: "images/warriors/valkyrie/BG_Valkyrie.png",
+  img: "images/warriors/valkyrie/Warrior_Valkyrie.png"
+}];
+
+const mages = [{
+  name: "Аркана",
+  backgroundImg: "images/mages/arcanist/BG_Arcanist.png",
+  img: "images/mages/arcanist/Mage_Arcana.png"
+}, {
+  name: "Чародейка",
+  backgroundImg: "images/mages/sorceress/BG_Sorceress.png",
+  img: "images/mages/sorceress/Mage_Sorceress.png"
+}, {
+  name: "Менестрель",
+  backgroundImg: "images/mages/bard/BG_Bard.png",
+  img: "images/mages/bard/Mage_Bard.png"
+}, {
+  name: "Призывательница",
+  backgroundImg: "images/mages/summoner/BG_Summoner.png",
+  img: "images/mages/summoner/Mage_Summoner.png"
+}];
+
+const fighters = [{
+  name: "Стальной Кулак",
+  backgroundImg: "images/fighters/breaker/BG_Breaker.png",
+  img: "images/fighters/breaker/Fighter_Breaker.png"
+}, {
+  name: "Мастер копья",
+  backgroundImg: "images/fighters/glaivier/BG_Glaivier.png",
+  img: "images/fighters/glaivier/Fighter_Glaivier.png"
+}, {
+  name: "Ки-мастер",
+  backgroundImg: "images/fighters/soulfist/BG_Soulfist.png",
+  img: "images/fighters/soulfist/Fighter_Soulfist.png"
+}, {
+  name: "Дуалист",
+  backgroundImg: "images/fighters/scrapper/BG_Scrapper.png",
+  img: "images/fighters/scrapper/Fighter_Scrapper.png"
+}, {
+  name: "Аватар",
+  backgroundImg: "images/fighters/wardancer/BG_Wardancer.png",
+  img: "images/fighters/wardancer/Fighter_Wardancer.png"
+}, {
+  name: "Тайгон",
+  backgroundImg: "images/fighters/striker/BG_Striker.png",
+  img: "images/fighters/striker/Fighter_Striker.png"
+}];
+
+const gunners = [{
+  name: "Механист",
+  backgroundImg: "images/gunners/artillerist/BG_Artillerist.png",
+  img: "images/gunners/artillerist/Gunner_Artillerist.png"
+}, {
+  name: "Охотник на демонов",
+  backgroundImg: "images/gunners/deadeye/BG_Deadeye.png",
+  img: "images/gunners/deadeye/Gunner_Deadeye.png"
+}, {
+  name: "Рейнджер",
+  backgroundImg: "images/gunners/sharpshooter/BG_Sharpshooter.png",
+  img: "images/gunners/sharpshooter/Gunner_Sharpshooter.png"
+}, {
+  name: "Агент С.К.А.У.Т.",
+  backgroundImg: "images/gunners/machinist/BG_Scouter.png",
+  img: "images/gunners/machinist/Gunner_Scouter.png"
+}, {
+  name: "Охотница на демонов",
+  backgroundImg: "images/gunners/gunslinger/BG_Gunslinger.png",
+  img: "images/gunners/gunslinger/Gunner_Gunslinger.png"
+}];
+
+const assassins = [{
+  name: "Фурия",
+  backgroundImg: "images/assassins/shadowhunter/BG_Shadowhunter.png",
+  img: "images/assassins/shadowhunter/Assassin_Shadowhunter.png"
+}, {
+  name: "Клинок смерти",
+  backgroundImg: "images/assassins/deathblade/BG_Deathblade.png",
+  img: "images/assassins/deathblade/Assassin_Deathblade.png"
+}, {
+  name: "Жнец",
+  backgroundImg: "images/assassins/reaper/BG_Reaper.png",
+  img: "images/assassins/reaper/Assassin_Reaper.png"
+}, {
+  name: "Пожирательница душ",
+  backgroundImg: "images/assassins/souleater/BG_Souleater.png",
+  img: "images/assassins/souleater/Assassin_Souleater.png"
+}];
+
+const specialists = [{
+  name: "Друид",
+  backgroundImg: "images/specialists/wildsoul/BG_Wildsoul.png",
+  img: "images/specialists/wildsoul/Specialist_Wildsoul.png"
+}, {
+  name: "Художница",
+  backgroundImg: "images/specialists/artist/BG_Artist.png",
+  img: "images/specialists/artist/Specialist_Artist.png"
+}, {
+  name: "Аэромант",
+  backgroundImg: "images/specialists/aeromancer/BG_Aeromancer.png",
+  img: "images/specialists/aeromancer/Specialist_Aeromancer.png"
+}];
+
+const guardianKnights = [{
+  name: "Рыцарь-Хранитель",
+  backgroundImg: "images/guardian knights/guardian knight/BG_GK.webp",
+  img: "images/guardian knights/guardian knight/emblem_dragon_knight.png"
+}];
---
+++ scripts/general.js
@@ -0,0 +1,152 @@
+let typesHTML = '';
+
+types.forEach((type) => {
+  typesHTML += `
+      <div class="type-container">
+        <div class="type-title">${type.nameRU}</div>
+        <div class="class-cards ${type.nameJS}"></div>
+      </div>
+  `;
+});
+
+document.querySelector('.js-types-container').innerHTML = typesHTML;
+
+let warriorsHTML = '';
+
+warriors.forEach((warrior) => {
+  warriorsHTML += `
+          <a class="class-card">
+            <div class="class-title">${warrior.name}</div>
+            <img
+              class="class-bg-img"
+              src="${warrior.backgroundImg}"
+            />
+            <img
+              class="class-img"
+              src="${warrior.img}"
+            />
+          </a>
+  `;
+});
+
+document.querySelector('.js-warriors-container').innerHTML = warriorsHTML;
+
+let magesHTML = '';
+
+mages.forEach((mage) => {
+  magesHTML += `
+          <a class="class-card">
+            <div class="class-title">${mage.name}</div>
+            <img
+              class="class-bg-img"
+              src="${mage.backgroundImg}"
+            />
+            <img
+              class="class-img"
+              src="${mage.img}"
+            />
+          </a>
+  `;
+});
+
+document.querySelector('.js-mages-container').innerHTML = magesHTML;
+
+let fightersHTML = '';
+
+fighters.forEach((fighter) => {
+  fightersHTML += `
+          <a class="class-card">
+            <div class="class-title">${fighter.name}</div>
+            <img
+              class="class-bg-img"
+              src="${fighter.backgroundImg}"
+            />
+            <img
+              class="class-img"
+              src="${fighter.img}"
+            />
+          </a>
+  `;
+});
+
+document.querySelector('.js-fighters-container').innerHTML = fightersHTML;
+
+let gunnersHTML = '';
+
+gunners.forEach((gunner) => {
+  gunnersHTML += `
+          <a class="class-card">
+            <div class="class-title">${gunner.name}</div>
+            <img
+              class="class-bg-img"
+              src="${gunner.backgroundImg}"
+            />
+            <img
+              class="class-img"
+              src="${gunner.img}"
+            />
+          </a>
+  `;
+});
+
+document.querySelector('.js-gunners-container').innerHTML = gunnersHTML;
+
+let assassinsHTML = '';
+
+assassins.forEach((assassin) => {
+  assassinsHTML += `
+          <a class="class-card">
+            <div class="class-title">${assassin.name}</div>
+            <img
+              class="class-bg-img"
+              src="${assassin.backgroundImg}"
+            />
+            <img
+              class="class-img"
+              src="${assassin.img}"
+            />
+          </a>
+  `;
+});
+
+document.querySelector('.js-assassins-container').innerHTML = assassinsHTML;
+
+let specialistsHTML = '';
+
+specialists.forEach((specialist) => {
+  specialistsHTML += `
+          <a class="class-card">
+            <div class="class-title">${specialist.name}</div>
+            <img
+              class="class-bg-img"
+              src="${specialist.backgroundImg}"
+            />
+            <img
+              class="class-img"
+              src="${specialist.img}"
+            />
+          </a>
+  `;
+});
+
+document.querySelector('.js-specialists-container').innerHTML = specialistsHTML;
+
+let guardianKnightsHTML = '';
+
+guardianKnights.forEach((guardianKnight) => {
+  guardianKnightsHTML += `
+          <a class="class-card">
+            <div class="class-title">${guardianKnight.name}</div>
+            <img
+              class="class-bg-img"
+              src="${guardianKnight.backgroundImg}"
+            />
+            <img
+              class="class-img"
+              src="${guardianKnight.img}"
+            />
+          </a>
+  `;
+});
+
+document.querySelector('.js-guardianKnights-container').innerHTML = guardianKnightsHTML;
